e9df68d361e4166ec06a89d6473a13301be468e4
Joshua Forisha <jforisha@softlayer.com>
2015-07-23 14:33:44 -0500

    Minor tweaks to streamService methods

--- README.md
+++ README.md
@@ -1,25 +1,49 @@
-# Ember-stream
+# Ember Stream [![NPM Version](https://img.shields.io/npm/v/ember-stream.svg?style=flat-square)](https://www.npmjs.com/package/ember-stream)
 
-This README outlines the details of collaborating on this Ember addon.
+Observable streams addon for Ember CLI projects, using [RxJS](https://github.com/Reactive-Extensions/RxJS).
+
+## Use in Ember CLI
+
+- `ember install ember-stream`
+
+### Stream Service
+
+The primary object available is the stream service, located at `/addon/services/stream.js`. This service has the following methods available:
+
+#### Rx
+
+This is an alias to the main Rx library, so that any object using the streamService has direct access to the native Reactive functionality.
+
+#### createStream( func )
+
+Create an observable stream from a function definition. This currently just wraps *Rx.Observable.create()*, but in the future will support more Ember-friendly patterns.
+
+#### findStream( streamName )
+
+Lookup and return a named stream registered on the streamService.
+
+#### registerStream( streamsObjectOrName, stream )
+
+Register an observable stream to a referenceable name. Any observers that are awaiting subscription to this stream will be subscribed at this point.
+
+There are two ways to call this method:
+
+- Pass in a single name and stream instance
+- Pass in a key-value hash, where the key is the stream name, and the value is the stream instance
+
+#### subscribeTo( streamName, observerOrOnNext, onError, onCompleted )
+
+Attempt to subscribe an observer or series of callbacks to an observable stream. If no observer is registered under the `streamName`, then the subscription will not be setup until the stream is registered.
+
+There are two ways to call this method:
+
+- Pass in the `streamName` and at least one callback function
+- Pass in the `streamName` and an Rx.Observer object
 
 ## Installation
 
-* `git clone` this repository
-* `npm install`
-* `bower install`
-
-## Running
-
-* `ember server`
-* Visit your app at http://localhost:4200.
-
-## Running Tests
-
-* `ember test`
-* `ember test --server`
-
-## Building
-
-* `ember build`
+- `git clone` this repository
+- `npm install`
+- `bower install`
 
 For more information on using ember-cli, visit [http://www.ember-cli.com/](http://www.ember-cli.com/).
--- addon/services/stream.js
+++ addon/services/stream.js
@@ -12,21 +12,21 @@ let subscriptions = {};
 export default Ember.Service.extend({
 
     /**
+     * An alias to the ReactiveX library itself
+     *
+     * @type {Object}
+     */
+    Rx: window.Rx,
+
+    /**
      * Create an observable stream from a function definition
      *
      * @function
      * @param {Function} func - The function to create the stream from
-     * @param {String} [name] - A name to register the stream to
      * @returns {rx/Observable} - The created observable stream
      */
-    createStream( func, name ) {
-        const stream = this.Rx.Observable.create( func );
-
-        if ( name ) {
-            this.registerStream( name, stream );
-        }
-
-        return stream;
+    createStream( func ) {
+        return this.Rx.Observable.create( func );
     },
 
     /**
@@ -54,25 +54,24 @@ export default Ember.Service.extend({
      * @param {Object|String} streamsObjectOrName - Either an object containing
      *        key-value pairs of multiple stream definitions, or a single string
      *        used to register the `observable` name to
-     * @param {rx/Observable} [observable] - A single observable object, if the
+     * @param {rx/Observable} [stream] - A single observable object, if the
      *        first argument is a string
      * @returns {Boolean} - True unless an error is encountered
      */
-    registerStream( streamsObjectOrName, observable ) {
-        if ( 'string' === Ember.typeOf( streamsObjectOrName ) && observable ) {
+    registerStream( streamsObjectOrName, stream ) {
+        if ( 'string' === Ember.typeOf( streamsObjectOrName ) && stream ) {
             const streamName = streamsObjectOrName;
 
             if ( streams.hasOwnProperty( streamName ) ) {
                 Ember.Logger.warn( `Stream "${streamName}" already exists` );
-                return false;
             }
 
-            streams[ streamName ] = observable;
+            streams[ streamName ] = stream;
 
             // Set up subscriptions for observers waiting for this named stream
             if ( subscriptions.hasOwnProperty( streamName ) ) {
                 for ( const subscription of subscriptions[ streamName ] ) {
-                    observable.subscribe.apply( observable, subscription );
+                    stream.subscribe.apply( stream, subscription );
                 }
 
                 delete subscriptions[ streamName ];
@@ -97,13 +96,6 @@ export default Ember.Service.extend({
 
         return false;
     },
-
-    /**
-     * An alias to the ReactiveX library itself
-     *
-     * @type {Object}
-     */
-    Rx: window.Rx,
 
     /**
      * Attempts to subscribe an observer (or series of callbacks) to an
